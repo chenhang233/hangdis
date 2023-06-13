df14958c3fd65f9ea047132e58fa6c90dcd26da8
chenhang <1973433180@qq.com>
2023-06-13 16:49:26 +0800

    test: 测试hash code

---
+++ test/datastruct/data_test.go
@@ -0,0 +1,25 @@
+package datastruct
+
+import (
+	"fmt"
+	"testing"
+)
+
+const prime32 = uint32(16777619)
+
+func fnv32(key string) uint32 {
+	hash := uint32(2166136261)
+	for i := 0; i < len(key); i++ {
+		hash *= prime32
+		hash ^= uint32(key[i])
+	}
+	return hash
+}
+
+func TestDictSpread(t *testing.T) {
+	size := 50
+	u := fnv32("runoobkey")
+	fmt.Println("code: ", u)
+	test := uint32(size-1) & u
+	fmt.Println("index: ", test)
+}
