73ed4f4f10eedee0b3f2c8b377c71a3bca21fd32
chenhang233 <1973433180@qq.com>
2023-06-22 11:14:04 +0800

    fix: client parse *array

--- redis/client/cmd/main.go
+++ redis/client/cmd/main.go
@@ -84,6 +84,16 @@ func parseReplyType(response redis.Reply) {
 	case ':':
 		reply := response.(*protocol.IntReply)
 		fmt.Println(fmt.Sprintf(": %s", utils.Blue(strconv.FormatInt(reply.Code, 10))))
+	case '*':
+		reply := response.(*protocol.MultiBulkReply)
+		n := len(reply.Args)
+		for i := 0; i < n; i++ {
+			t := string(reply.Args[i])
+			if len(t) == 0 {
+				t = "null"
+			}
+			fmt.Println(fmt.Sprintf("* %d: %s", i, utils.Yellow(t)))
+		}
 	default:
 		fmt.Println(fmt.Sprintf("can not parse %s", utils.Purple(string(response.ToBytes()))))
 	}
