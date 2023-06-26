5df06aef38e8f2fea4084d1859605e0201e8211b
chenhang233 <1973433180@qq.com>
2023-06-26 12:21:55 +0800

    feat: list LPOP RPOP RPOPLPUSH LREM

--- database/list.go
+++ database/list.go
@@ -6,6 +6,7 @@ import (
 	"hangdis/interface/redis"
 	"hangdis/redis/protocol"
 	"hangdis/utils"
+	"strconv"
 )
 
 func (db *DB) getAsList(key string) (List.List, redis.ErrorReply) {
@@ -52,6 +53,71 @@ func undoRPush(db *DB, args [][]byte) []CmdLine {
 		cmdLines = append(cmdLines, utils.ToCmdLine("RPOP", key))
 	}
 	return cmdLines
+}
+
+var lPushCmd = []byte("LPUSH")
+
+func undoLPop(db *DB, args [][]byte) []CmdLine {
+	key := string(args[0])
+	list, errReply := db.getAsList(key)
+	if errReply != nil {
+		return nil
+	}
+	if list == nil || list.Len() == 0 {
+		return nil
+	}
+	element, _ := list.Get(0).([]byte)
+	return []CmdLine{
+		{
+			lPushCmd,
+			args[0],
+			element,
+		},
+	}
+}
+
+var rPushCmd = []byte("RPUSH")
+
+func undoRPop(db *DB, args [][]byte) []CmdLine {
+	key := string(args[0])
+	list, errReply := db.getAsList(key)
+	if errReply != nil {
+		return nil
+	}
+	if list == nil || list.Len() == 0 {
+		return nil
+	}
+	element, _ := list.Get(list.Len() - 1).([]byte)
+	return []CmdLine{
+		{
+			rPushCmd,
+			args[0],
+			element,
+		},
+	}
+}
+
+func undoRPopLPush(db *DB, args [][]byte) []CmdLine {
+	sourceKey := string(args[0])
+	list, errReply := db.getAsList(sourceKey)
+	if errReply != nil {
+		return nil
+	}
+	if list == nil || list.Len() == 0 {
+		return nil
+	}
+	element, _ := list.Get(list.Len() - 1).([]byte)
+	return []CmdLine{
+		{
+			rPushCmd,
+			args[0],
+			element,
+		},
+		{
+			[]byte("LPOP"),
+			args[1],
+		},
+	}
 }
 
 func execLPush(db *DB, args [][]byte) redis.Reply {
@@ -116,9 +182,161 @@ func execRPushX(db *DB, args [][]byte) redis.Reply {
 	return protocol.MakeIntReply(int64(list.Len()))
 }
 
+func execLPop(db *DB, args [][]byte) redis.Reply {
+	key := string(args[0])
+	list, err := db.getAsList(key)
+	if err != nil {
+		return err
+	}
+	if list == nil {
+		return protocol.MakeEmptyMultiBulkReply()
+	}
+	rn := 1
+	if len(args) > 1 {
+		n, err2 := strconv.ParseInt(string(args[1]), 10, 64)
+		if err2 != nil {
+			return protocol.MakeErrReply("ERR invalid count")
+		}
+		rn = int(n)
+		if rn <= 0 {
+			return protocol.MakeErrReply("ERR count less than 0")
+		}
+	}
+	if rn == 1 {
+		val := list.Remove(0).([]byte)
+		if list.Len() == 0 {
+			db.Remove(key)
+		}
+		db.addAof(utils.ToCmdLine3("lpop", args...))
+		return protocol.MakeBulkReply(val)
+	}
+	res := make([][]byte, rn)
+	for i := 0; i < rn; i++ {
+		if list.Len() == 0 {
+			break
+		}
+		val := list.Remove(0).([]byte)
+		res[i] = val
+	}
+	db.addAof(utils.ToCmdLine3("lpop", args...))
+	return protocol.MakeMultiBulkReply(res)
+}
+
+func execRPop(db *DB, args [][]byte) redis.Reply {
+	key := string(args[0])
+	list, err := db.getAsList(key)
+	if err != nil {
+		return err
+	}
+	if list == nil {
+		return protocol.MakeEmptyMultiBulkReply()
+	}
+	rn := 1
+	if len(args) > 1 {
+		n, err2 := strconv.ParseInt(string(args[1]), 10, 64)
+		if err2 != nil {
+			return protocol.MakeErrReply("ERR invalid count")
+		}
+		rn = int(n)
+		if rn <= 0 {
+			return protocol.MakeErrReply("ERR count less than 0")
+		}
+	}
+	if rn == 1 {
+		val := list.RemoveLast().([]byte)
+		if list.Len() == 0 {
+			db.Remove(key)
+		}
+		db.addAof(utils.ToCmdLine3("rpop", args...))
+		return protocol.MakeBulkReply(val)
+	}
+	res := make([][]byte, rn)
+	for i := 0; i < rn; i++ {
+		if list.Len() == 0 {
+			break
+		}
+		val := list.RemoveLast().([]byte)
+		res[i] = val
+	}
+	db.addAof(utils.ToCmdLine3("rpop", args...))
+	return protocol.MakeMultiBulkReply(res)
+}
+
+func prepareRPopLPush(args [][]byte) ([]string, []string) {
+	return []string{
+		string(args[0]),
+		string(args[1]),
+	}, nil
+}
+
+func execRPopLPush(db *DB, args [][]byte) redis.Reply {
+	sourceKey := string(args[0])
+	destKey := string(args[1])
+	list1, err := db.getAsList(sourceKey)
+	if err != nil {
+		return err
+	}
+	if list1 == nil {
+		return protocol.MakeEmptyMultiBulkReply()
+	}
+	list2, _, err := db.getOrInitList(destKey)
+	if err != nil {
+		return err
+	}
+	last := list1.RemoveLast().([]byte)
+	list2.Insert(0, last)
+	if list1.Len() == 0 {
+		db.Remove(sourceKey)
+	}
+	db.addAof(utils.ToCmdLine3("rpoplpush", args...))
+	return protocol.MakeBulkReply(last)
+}
+
+func execLRem(db *DB, args [][]byte) redis.Reply {
+	key := string(args[0])
+	count64, err := strconv.ParseInt(string(args[1]), 10, 64)
+	if err != nil {
+		return protocol.MakeErrReply("ERR value is not an integer or out of range")
+	}
+	count := int(count64)
+	value := args[2]
+	list, err2 := db.getAsList(key)
+	if err2 != nil {
+		return err2
+	}
+	if list == nil {
+		return protocol.MakeIntReply(0)
+	}
+	var removed int
+	if count < 0 {
+		removed = list.ReverseRemoveByVal(func(a any) bool {
+			return utils.Equals(a, value)
+		}, -count)
+	} else if count > 0 {
+		removed = list.RemoveByVal(func(a any) bool {
+			return utils.Equals(a, value)
+		}, count)
+	} else {
+		removed = list.RemoveAllByVal(func(a any) bool {
+			return utils.Equals(a, value)
+		})
+	}
+	if list.Len() == 0 {
+		db.Remove(key)
+	}
+	if removed > 0 {
+		db.addAof(utils.ToCmdLine3("lrem", args...))
+	}
+	return protocol.MakeIntReply(int64(removed))
+}
+
 func init() {
-	RegisterCommand("LPush", execLPush, writeFirstKey, undoLPush, -3, flagWrite)
-	RegisterCommand("LPushX", execLPushX, writeFirstKey, undoLPush, -3, flagWrite)
-	RegisterCommand("RPush", execRPush, writeFirstKey, undoRPush, -3, flagWrite)
-	RegisterCommand("RPushX", execRPushX, writeFirstKey, undoRPush, -3, flagWrite)
+	RegisterCommand("LPUSH", execLPush, writeFirstKey, undoLPush, -3, flagWrite)
+	RegisterCommand("LPUSHX", execLPushX, writeFirstKey, undoLPush, -3, flagWrite)
+	RegisterCommand("RPUSH", execRPush, writeFirstKey, undoRPush, -3, flagWrite)
+	RegisterCommand("RPUSHX", execRPushX, writeFirstKey, undoRPush, -3, flagWrite)
+	RegisterCommand("LPOP", execLPop, writeFirstKey, undoLPop, -2, flagWrite)
+	RegisterCommand("RPOP", execRPop, writeFirstKey, undoRPop, -2, flagWrite)
+	RegisterCommand("RPOPLPUSH", execRPopLPush, prepareRPopLPush, undoRPopLPush, 3, flagWrite)
+	RegisterCommand("LREM", execLRem, writeFirstKey, rollbackFirstKey, 4, flagWrite)
 }
--- utils/utils.go
+++ utils/utils.go
@@ -1,5 +1,9 @@
 package utils
 
+import (
+	"bytes"
+)
+
 func ConvertRange(start int64, end int64, size int64) (int, int) {
 	if start < -size {
 		return -1, -1
@@ -21,4 +25,13 @@ func ConvertRange(start int64, end int64, size int64) (int, int) {
 		return -1, -1
 	}
 	return int(start), int(end)
+}
+
+func Equals(a any, b any) bool {
+	b1, ok1 := a.([]byte)
+	b2, ok2 := b.([]byte)
+	if ok1 && ok2 {
+		return bytes.Equal(b1, b2)
+	}
+	return a == b
 }
