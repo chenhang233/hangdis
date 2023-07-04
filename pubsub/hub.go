8a4ebd4994d722f3c8182787ebf3aa0df926ff85
chenhang233 <1973433180@qq.com>
2023-07-04 15:45:23 +0800

    feat: pubsub init

--- pubsub/hub.go
+++ pubsub/hub.go
@@ -1,1 +1,13 @@
 package pubsub
+
+import Dict "hangdis/datastruct/dict"
+
+type Hub struct {
+	subs Dict.Dict
+}
+
+func MakeHub() *Hub {
+	return &Hub{
+		subs: Dict.MakeConcurrent(3),
+	}
+}
---
+++ pubsub/pubsub.go
@@ -0,0 +1,41 @@
+package pubsub
+
+import (
+	"hangdis/interface/redis"
+	"hangdis/redis/protocol"
+	"strconv"
+)
+
+var (
+	_subscribe         = "subscribe"
+	_unsubscribe       = "unsubscribe"
+	messageBytes       = []byte("message")
+	unSubscribeNothing = []byte("*3\r\n$11\r\nunsubscribe\r\n$-1\n:0\r\n")
+)
+
+func makeMsg(t string, channel string, code int64) []byte {
+	return []byte("*3\r\n$" + strconv.FormatInt(int64(len(t)), 10) + protocol.CRLF + t + protocol.CRLF +
+		"$" + strconv.FormatInt(int64(len(channel)), 10) + protocol.CRLF + channel + protocol.CRLF +
+		":" + strconv.FormatInt(code, 10) + protocol.CRLF)
+}
+
+func subscribe0(hub *Hub, channel string, client redis.Connection) bool {
+	client.Subscribe(channel)
+	val, exists := hub.subs.Get(channel)
+	//var subscribers *list.LinkedList
+	if exists {
+
+	} else {
+
+	}
+}
+
+func unsubscribe0(hub *Hub, channel string, client redis.Connection) bool {}
+
+func UnsubscribeAll(hub *Hub, c redis.Connection) {}
+
+func UnSubscribe(db *Hub, c redis.Connection, args [][]byte) redis.Reply {}
+
+func Publish(hub *Hub, args [][]byte) redis.Reply {}
+
+func Subscribe(hub *Hub, c redis.Connection, args [][]byte) redis.Reply {}
