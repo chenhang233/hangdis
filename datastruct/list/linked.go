ddd1401a22d672072792e0274b68cb14cef02a42
chenhang233 <1973433180@qq.com>
2023-07-06 14:12:09 +0800

    core: linked finish

--- datastruct/list/linked.go
+++ datastruct/list/linked.go
@@ -7,7 +7,7 @@ type LinkedList struct {
 }
 
 type node struct {
-	val  interface{}
+	val  any
 	prev *node
 	next *node
 }
@@ -98,14 +98,158 @@ func (list *LinkedList) Insert(index int, val any) {
 	return
 }
 
-func (list *LinkedList) removeNode(n *node) {}
+func (list *LinkedList) removeNode(n *node) {
+	if n.prev == nil {
+		list.first = n.next
+	} else {
+		n.prev.next = n.next
+	}
+	if n.next == nil {
+		list.last = n.prev
+	} else {
+		n.next.prev = n.prev
+	}
+	n.prev = nil
+	n.next = nil
+	list.size--
+	return
+}
 
-func (list *LinkedList) Remove(index int) (val any)                          {}
-func (list *LinkedList) RemoveLast() (val any)                               {}
-func (list *LinkedList) RemoveAllByVal(expected Expected) int                {}
-func (list *LinkedList) RemoveByVal(expected Expected, count int) int        {}
-func (list *LinkedList) ReverseRemoveByVal(expected Expected, count int) int {}
-func (list *LinkedList) Len() int                                            {}
-func (list *LinkedList) ForEach(consumer Consumer)                           {}
-func (list *LinkedList) Contains(expected Expected) bool                     {}
-func (list *LinkedList) Range(start int, stop int) []interface{}             {}
+func (list *LinkedList) Remove(index int) (val any) {
+	if list == nil {
+		panic("list is nil")
+	}
+	if index < 0 || index >= list.size {
+		panic("index out of bound")
+	}
+	n := list.find(index)
+	list.removeNode(n)
+	return n.val
+}
+func (list *LinkedList) RemoveLast() (val any) {
+	if list == nil {
+		panic("list is nil")
+	}
+	if list.last == nil {
+		return nil
+	}
+	last := list.last
+	list.removeNode(last)
+	return last.val
+}
+func (list *LinkedList) RemoveAllByVal(expected Expected) int {
+	if list == nil {
+		panic("list is nil")
+	}
+	n := list.first
+	removed := 0
+	var nextNode *node
+	for n != nil {
+		nextNode = n.next
+		if expected(n.val) {
+			list.removeNode(n)
+			removed++
+		}
+		n = nextNode
+	}
+	return removed
+}
+func (list *LinkedList) RemoveByVal(expected Expected, count int) int {
+	if list == nil {
+		panic("list is nil")
+	}
+	n := list.first
+	removed := 0
+	var nextNode *node
+	for n != nil {
+		nextNode = n.next
+		if expected(n.val) {
+			list.removeNode(n)
+			removed++
+			if removed == count {
+				break
+			}
+		}
+		n = nextNode
+	}
+	return removed
+}
+func (list *LinkedList) ReverseRemoveByVal(expected Expected, count int) int {
+	if list == nil {
+		panic("list is nil")
+	}
+	n := list.last
+	removed := 0
+	var prevNode *node
+	for n != nil {
+		prevNode = n.prev
+		if expected(n.val) {
+			list.removeNode(n)
+			removed++
+		}
+		if removed == count {
+			break
+		}
+		n = prevNode
+	}
+	return removed
+}
+func (list *LinkedList) Len() int {
+	if list == nil {
+		panic("list is nil")
+	}
+	return list.size
+}
+func (list *LinkedList) ForEach(consumer Consumer) {
+	if list == nil {
+		panic("list is nil")
+	}
+	n := list.first
+	i := 0
+	for n != nil {
+		goNext := consumer(i, n.val)
+		if !goNext {
+			break
+		}
+		i++
+		n = n.next
+	}
+}
+func (list *LinkedList) Contains(expected Expected) bool {
+	contains := false
+	list.ForEach(func(i int, actual any) bool {
+		if expected(actual) {
+			contains = true
+			return false
+		}
+		return true
+	})
+	return contains
+}
+func (list *LinkedList) Range(start int, stop int) []any {
+	if list == nil {
+		panic("list is nil")
+	}
+	if start < 0 || start >= list.size {
+		panic("`start` out of range")
+	}
+	if stop < start || stop > list.size {
+		panic("`stop` out of range")
+	}
+	sliceSize := stop - start
+	slice := make([]any, sliceSize)
+	n := list.first
+	i := 0
+	sliceIndex := 0
+	for n != nil {
+		if i >= start && i < stop {
+			slice[sliceIndex] = n.val
+			sliceIndex++
+		} else if i >= stop {
+			break
+		}
+		i++
+		n = n.next
+	}
+	return slice
+}
