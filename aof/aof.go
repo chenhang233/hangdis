04481d05f84021a7297298b5823be44ca0b716d2
chenhang233 <1973433180@qq.com>
2023-07-08 15:15:34 +0800

    feat: aof perSister struct

---
+++ aof/aof.go
@@ -0,0 +1,40 @@
+package aof
+
+import (
+	"context"
+	"hangdis/interface/database"
+	"os"
+	"sync"
+)
+
+const (
+	aofQueueSize = 1 << 16
+)
+
+type Listener interface {
+	Callback([]CmdLine)
+}
+
+type CmdLine = [][]byte
+
+type payload struct {
+	cmdLine CmdLine
+	dbIndex int
+	wg      *sync.WaitGroup
+}
+
+type PerSister struct {
+	ctx         context.Context
+	cancel      context.CancelFunc
+	db          database.DBEngine
+	tmpDBMaker  func() database.DBEngine
+	aofChan     chan *payload
+	aofFile     *os.File
+	aofFilename string
+	aofFsync    string
+	aofFinished chan struct{}
+	pausingAof  sync.Mutex
+	currentDB   int
+	listeners   map[Listener]struct{}
+	buffer      []CmdLine
+}
