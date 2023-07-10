2ef3f1c379f660d5c7e9282b2216e67ea1415aca
chenhang233 <1973433180@qq.com>
2023-07-10 15:34:58 +0800

    feat： aof NewPerSister

--- aof/aof.go
+++ aof/aof.go
@@ -2,13 +2,22 @@ package aof
 
 import (
 	"context"
+	"fmt"
 	"hangdis/interface/database"
+	"hangdis/utils"
+	"hangdis/utils/logs"
 	"os"
+	"strings"
 	"sync"
+	"time"
 )
 
 const (
 	aofQueueSize = 1 << 16
+)
+
+const (
+	FsyncEverySec = "everysec"
 )
 
 type Listener interface {
@@ -40,9 +49,68 @@ type PerSister struct {
 }
 
 func NewPerSister(db database.DBEngine, filename string, load bool, fsync string, tmpDBMaker func() database.DBEngine) (*PerSister, error) {
-
+	p := &PerSister{}
+	p.aofFilename = filename
+	p.aofFsync = strings.ToLower(fsync)
+	p.db = db
+	p.tmpDBMaker = tmpDBMaker
+	p.currentDB = 0
+	if load {
+		p.LoadAof(0)
+	}
+	aofFile, err := os.OpenFile(p.aofFilename, os.O_APPEND|os.O_CREATE|os.O_RDWR, os.ModePerm)
+	if err != nil {
+		return nil, err
+	}
+	p.aofFile = aofFile
+	p.aofChan = make(chan *payload, aofQueueSize)
+	p.aofFinished = make(chan struct{})
+	p.listeners = make(map[Listener]struct{})
+	go func() {
+		p.listenCmd()
+	}()
+	ctx, cancel := context.WithCancel(context.Background())
+	p.ctx = ctx
+	p.cancel = cancel
+	if p.aofFsync == FsyncEverySec {
+		p.fsyncEverySecond()
+	}
+	return p, nil
 }
 
 func (p *PerSister) LoadAof(maxBytes int) {
 
+}
+
+func (p *PerSister) listenCmd() {
+	for pc := range p.aofChan {
+		p.writeAof(pc)
+	}
+	p.aofFinished <- struct{}{}
+}
+
+func (p *PerSister) writeAof(payload *payload) {
+
+}
+
+func (p *PerSister) Fsync() {
+	p.pausingAof.Lock()
+	if err := p.aofFile.Sync(); err != nil {
+		logs.LOG.Error.Println(utils.Red(fmt.Sprintf("fsync failed: %v", err)))
+	}
+	p.pausingAof.Unlock()
+}
+
+func (p *PerSister) fsyncEverySecond() {
+	ticker := time.NewTicker(time.Second)
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				p.Fsync()
+			case <-p.ctx.Done():
+				return
+			}
+		}
+	}()
 }
--- test/tcp_test.go
+++ test/tcp_test.go
@@ -1,8 +1,11 @@
 package test
 
 import (
+	"context"
+	"fmt"
 	"os"
 	"testing"
+	"time"
 )
 
 func TestMap(t *testing.T) {
@@ -20,4 +23,16 @@ func TestTempFile(t *testing.T) {
 	}
 	temp.WriteString("hello world")
 	temp.Close()
+}
+
+func TestContext(t *testing.T) {
+	ctx := context.Background()
+	go func() {
+		time.Sleep(time.Second)
+		//done := ctx.Done()
+	}()
+	select {
+	case <-ctx.Done():
+		fmt.Println("done")
+	}
 }
